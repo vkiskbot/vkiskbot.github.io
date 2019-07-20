16a3f78af112d725ab5aed9904ee062797e3120b
vkiskbot <53118918+vkiskbot@users.noreply.github.com>
2019-07-20 16:53:32 +0300

    Add files via upload

---
+++ precache-manifest.a1601c153baa6592a7909e205343289c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25671de549836c45904e",
+    "url": "/static/js/main.0e1f4e6d.chunk.js"
+  },
+  {
+    "revision": "d0a2eca540454f2d45eb",
+    "url": "/static/js/2.a774b1b3.chunk.js"
+  },
+  {
+    "revision": "25671de549836c45904e",
+    "url": "/static/css/main.c8f06eb3.chunk.css"
+  },
+  {
+    "revision": "d0a2eca540454f2d45eb",
+    "url": "/static/css/2.a1d2347a.chunk.css"
+  },
+  {
+    "revision": "467c63fdbe713cd66e0ed2a92bd4ef84",
+    "url": "/index.html"
+  }
+];
